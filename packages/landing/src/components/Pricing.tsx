9794558113713ef266ca2f87482c044f468f04d9
Lucas McComb <lucasmccomb@gmail.com>
2026-02-18 11:37:23 -0500

    119: Match comparison table Suite header to golden pill style

--- packages/landing/src/components/Pricing.tsx
+++ packages/landing/src/components/Pricing.tsx
@@ -302,7 +302,7 @@ export function Pricing({ product, selectedApp, onAppChange, features = defaultF
                     <span>Darkly for Docs</span>
                   </th>
                   <th className="pricing-comparison-suite-col">
-                    <SuiteIcon size={14} />
+                    <SuiteIcon size={18} />
                     <span>Darkly Suite</span>
                   </th>
                 </tr>
